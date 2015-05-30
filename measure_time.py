7511cb13cd45c222e1842b9b5c69436bc34c98cc
Kamil Kijak <kamilkijak@gmail.com>
2015-05-30 14:57:59 +0200

    measures time

---
+++ measure_time.py
@@ -0,0 +1,13 @@
+__author__ = 'ubuntu'
+
+import multiply_my_way
+import multiply_with_numpy
+import timeit
+
+scripts = [multiply_my_way, multiply_with_numpy]
+
+if __name__ == '__main__':
+    for script in scripts:
+        import_command = "from " + str(script.__name__) + " import main"
+        t = timeit.Timer("main()", import_command)
+        print("Multiply with " + str(script.__name__) + ":  " + str(round(t.timeit(1), 4)) + "s")
